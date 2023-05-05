43a9e254d611738f0a80432b740220856bd98390
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-05-05 23:30:45 +1200

    added public folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
-/** @type {import('tailwindcss').Config} */
-export default {
+// eslint-disable-next-line no-undef
+module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   mode: "jit",
   theme: {
