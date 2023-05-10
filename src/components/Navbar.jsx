01c8319e7d022211f8e0a5eec261979590e1b8ef
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-05-10 23:22:58 +1200

    updating navbar, html.index and logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -12,7 +13,7 @@ const Navbar = () => {
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link 
-          to='/'
+          to="/"
           className="flex items-center gap-2"
           onClick={() => {
             setActive("");
@@ -20,8 +21,20 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-10 h-10 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">Michael Boitmann<span className="sm:block hidden">| Portfolio</span></p>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">Michael Boitmann &nbsp;
+            <span className="sm:block hidden"> | Portfolio</span>
+          </p>
+          <p className="text-red-500">asdsa</p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+        {navLinks.map((Link) => 
+          // eslint-disable-next-line react/jsx-key
+          <li>
+            <a href={`#${Link.id}`}>{Link.title}</a>
+          </li>
+        )}
+        </ul>
+
       </div>
     </nav>
   )
