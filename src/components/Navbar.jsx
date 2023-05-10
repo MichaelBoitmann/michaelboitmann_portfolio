1706589c4182adf286c1898461cb64cee77bd14f
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-05-11 10:35:51 +1200

    added logos for explorer tab, added threejs to json

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-10 h-10 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">Michael Boitmann &nbsp;
-            <span className="sm:block hidden"> | Portfolio</span>
+            <span className="md:block hidden"> | Portfolio</span>
           </p>
           <p className="text-red-500">asdsa</p>
         </Link>
