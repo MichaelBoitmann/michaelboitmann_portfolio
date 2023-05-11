5084a23e646f8790aa8ee0c2f7f0db2a0bbcf671
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-05-11 14:05:11 +1200

    created the short introduction to myself and formated with dot and line

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import { Suspense, useEffect, useState} from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF }  from '@react-three/drei';
+
+import CanvasLoader from '../Loader';
+
 
 const Computers = () => {
   return (
