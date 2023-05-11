a3185ea1b6c1f2e21050b470f2a433c579599ff4
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-05-11 13:43:12 +1200

    finish up navbar files with addiitonal toggle switch drop down menu when resized; started on Hero file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,8 @@ import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
-  const [active, setActive] = useState('')
+  const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
 
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
@@ -20,21 +21,61 @@ const Navbar = () => {
             window.scrollTo(0, 0)
           }}
         >
-          <img src={logo} alt="logo" className="w-10 h-10 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex">Michael Boitmann &nbsp;
+          <img src={logo} alt="logo" className="w-11 h-11 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">Michael &nbsp;
             <span className="md:block hidden"> | Portfolio</span>
-          </p>
-          <p className="text-red-500">asdsa</p>
+          </p>        
         </Link>
+
         <ul className="list-none hidden sm:flex flex-row gap-10">
-        {navLinks.map((Link) => 
-          // eslint-disable-next-line react/jsx-key
-          <li>
-            <a href={`#${Link.id}`}>{Link.title}</a>
-          </li>
-        )}
+          {navLinks.map((Link) => 
+            // eslint-disable-next-line react/jsx-key
+            <li
+              key={Link.id}
+              className={`${
+                active === Link.title 
+                ? "text-white" 
+                : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(Link.title)}
+            >
+              <a href={`#${Link.id}`}>{Link.title}</a>
+            </li>
+          )}
         </ul>
 
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu" 
+            className="w-[28px] h-[28px] object-contain cursor-pointer" 
+            onClick={() => setToggle(!toggle)}
+          />
+
+          <div 
+            className={`${
+              !toggle ? 'hidden' : 'flex' 
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className="list-none flex justify-end items-start flex-col gap-2">
+              {navLinks.map((Link) => (
+                // eslint-disable-next-line react/jsx-key
+                <li
+                  key={Link.id}
+                  className={`font-poppins cursor-pointer text-[16px] ${
+                    active === Link.title ? "text-white" : "text-secondary"
+                  }`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(Link.title);
+                  }}
+                >
+                  <a href={`#${Link.id}`}>{Link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   )
