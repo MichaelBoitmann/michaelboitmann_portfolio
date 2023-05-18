603b93b68b184db4b736f27cbfbe405ed94672ca
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-05-18 23:49:02 +1200

    added some imports to section wrapper

---
+++ src/hoc/SectionWrapper.jsx
@@ -0,0 +1,13 @@
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { staggerContainer } from '../utils/motion'; 
+
+const SectionWrapper = (Component, idName) => 
+function HOC() {
+  return (
+    <div>SectionWrapper</div>
+  )
+}
+
+export default SectionWrapper
