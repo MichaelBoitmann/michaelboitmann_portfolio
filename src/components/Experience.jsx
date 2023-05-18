f4fcce71a788a332357d8be17537262c87249f90
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-05-19 00:36:31 +1200

    working on experience profile from experience file; corrected about file from impot

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import { motion } from 'framer-motion';
+
+import 'react-vertical-timeline-component/style.min.css';
+
+import { styles } from '../styles';
+import { experiences } from '../constants';
+import { SectionWrapper } from '../utils/motion';
+import { textVariant } from '../utils/motion';
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubtext}>
+          What have I done so far
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          Work Experience
+        </h2>
+      </motion.div>
+
+      <div className="mt-20 flex flex-col">
+        <VerticalTimeline>
+          
+        </VerticalTimeline>
+      </div>
+
+    </>
   )
 }
 
