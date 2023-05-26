06f1fb841186d9198091f0da30aa9743dac7f678
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-05-26 23:52:40 +1200

    added meshStandardMaterial

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -15,7 +15,16 @@ const Ball = () => {
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1,1]} />
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffeset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal 
+          position={[0, 0, 0]}
+        />
       </mesh>
     </Float>
   )
