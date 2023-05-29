4938ce57c115d94f2c8423d022b9af760ca99f4d
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-05-29 23:50:04 +1200

    finalize projects file

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -41,6 +41,15 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
           <h3 className="text-white font-bold text-[24px]">{name}</h3>
           <p className="mt-2 text-secondary text-[16px]">{description}</p>
         </div>
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+
+            </p>
+          ))}
+
+        </div>
       </Tilt>
     </motion.div>
   )
@@ -50,10 +59,10 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} text-center`}>
+        <p className={`${styles.sectionSubText} text-left`}>
           My Work
         </p>
-        <h2 className={`${styles.sectionHeadText} text-center`}>
+        <h2 className={`${styles.sectionHeadText} text-left`}>
           Projects.
         </h2>
       </motion.div>
