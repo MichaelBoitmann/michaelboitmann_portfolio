7239a12926ca54e1c89bf8d217aabfe190e25ebb
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-06-02 05:54:51 +1200

    construct the earth canvas function

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import CanvasLoader from '../Loader';
 
 const Earth = () => {
   return (
     <div>Earth</div>
+  )
+}
+
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadowsframLoop='demand'
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{ }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+      </Suspense>
+    </Canvas>
   )
 }
 
