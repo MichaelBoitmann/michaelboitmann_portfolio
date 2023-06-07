c468687da07a0b1d0860c071e6d79f4ca482ee81
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-06-08 00:11:07 +1200

    corrected the spelling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]}"
-      > I&#39;m skilled software developer with experienc ein TypeScript and JavaScript, and expertise in frameworks like React, Node.js, and Three.js. I&#39;m a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let&#39;s work together to bring your ideas to life!
+      > I&#39;m skilled software developer with experience in TypeScript and JavaScript, and expertise in frameworks like React, Node.js, and Three.js. I&#39;m a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let&#39;s work together to bring your ideas to life!
       </motion.p>
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index ) => (
